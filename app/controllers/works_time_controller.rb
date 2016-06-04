3a80fc282f09a6f493088de7b157d9208ff85d0b
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-06-04 15:47:09 +0200

    set log_out for get method, prepare for work_time

--- app/controllers/works_time_controller.rb
+++ app/controllers/works_time_controller.rb
@@ -1,8 +1,19 @@
 class WorksTimeController < ApplicationController
   def index
+    @works_time = WorkTime.all
   end
 
   def new
+    @work_time = WorkTime.new
+  end
+
+  def create
+    @new_work_time = WorkTime.new(work_time_parameters)
+    if @new_work_time.save
+      redirect_to(:controller => 'works_time', :action => 'index')
+    else
+      render('new')
+    end
   end
 
   def edit
