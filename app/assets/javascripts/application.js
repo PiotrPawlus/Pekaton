657c568b08bf572a3109cd1ce43e53ea761edbae
kubek93 <jakub.michnowicz@gmail.com>
2016-06-05 01:33:16 +0200

    Repair material design js, change css for work time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,33 +29,8 @@ $(document).ready(function() {
         $('body').css('overflow', 'visible');
     });
 
-    $('.single_task').find('select').addClass('form-control').addClass('select_single_task');
-
-    function startTime() {
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        var s = today.getSeconds();
-        m = checkTime(m);
-        s = checkTime(s);
-        document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
-        var t = setTimeout(startTime, 500);
-    }
-    function checkTime(i) {
-        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-        return i;
-    }
-    startTime();
-
-    // For material design
-    $.material.init();
-
-
-
-
-
-
-
+    $('.single_task').find('select').addClass('form-control');
+    $.material.init()
 });
 
 
