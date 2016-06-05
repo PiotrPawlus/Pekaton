a07e90d6e0acd9cf2bbba871143b08e0dd0f2edd
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-06-05 06:02:23 +0200

    start to validate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+        validates :email, :password, :password_confirmation, :name, :surname, :birth_date, presence: true
+
+        validates :email,
+                  email: true,
+                  length: { minimum: 1, maximum: 50, :message => "E-mail should have form 1 to 50 characters."}
+
+        validates :name,
+                  length: { minimum: 1, maximum: 40, :message => "Name should have from 1 to 50 characters."}
+
+        validates :surname,
+                  length: { minimum: 1, maximum: 40, :message => "Nazwisko musi zawierać od 1 do 50 znaków."}
 end
